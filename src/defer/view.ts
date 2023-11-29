30bfd514e20e1a60d40f4d4567b335db3207c016
flamrdevs <flamrdevs@gmail.com>
2023-11-29 14:53:37 +0700

    build: setup env and fix view

--- src/defer/view.ts
+++ src/defer/view.ts
@@ -1,12 +1,19 @@
 import { defer } from "@defer/client";
 
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      readonly VIEW_SECRET: string;
+    }
+  }
+}
+
 const view = async () => {
   const slice = async (name: string) => {
+    const body = new FormData();
+    body.append("secret", process.env.VIEW_SECRET);
     try {
-      await fetch(`https://view.flamrdevs.workers.dev/${name}/slice`, {
-        method: "PUT",
-        body: new FormData(),
-      });
+      await fetch(`https://view.flamrdevs.workers.dev/${name}/slice`, { method: "PUT", body });
       console.error(`[slice:${name}] ok`);
     } catch (error) {
       console.error(`[slice:${name}] failed`);
